544c836c28bb2e89cc6128899b108502b42a0fad
qk <250928251+quake-netizen@users.noreply.github.com>
2025-12-28 12:34:13 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,7 @@ import {onMounted, ref} from "vue";
 
 const info = ref(null)
 onMounted(() => {
-  const hash = window.location.hash; // 得到类似“#?tgWebAppData=xxxx”
-  const queryParams = new URLSearchParams(hash.substring(hash.indexOf('?')));
-  const rawInitData = queryParams.get('tgWebAppData'); // 这是您需要的原始字符串
-  info.value = rawInitData;
+  info.value = window.location
 })
 </script>
 
